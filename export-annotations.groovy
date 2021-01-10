eada48716b6225ee33cae539bf6cef65b33552e2
unknown <bli346@wisc.edu>
2021-01-10 15:55:46 -0600

    add bf image enhancement and generation roi from qupath

--- export-annotations.groovy
+++ export-annotations.groovy
@@ -1,9 +1,8 @@
-def annotations = getAnnotationObjects()
-boolean prettyPrint = true
-def gson = GsonTools.getInstance(prettyPrint)
-println gson.toJson(annotations)
+def rois = getAnnotationObjects().collect {it.getROI()}
+def gson = GsonTools.getInstance(true)
+println gson.toJson(rois)
 
 def name = getProjectEntry().getImageName() + '.json'
 def path = buildFilePath(PROJECT_BASE_DIR, 'annotation results', name)
 File file = new File(path)
-file.write(gson.toJson(annotations))
+file.write(gson.toJson(rois))
